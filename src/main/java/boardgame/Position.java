3abdc0487979b9f0c3b3bc283eacefe4039dc9b0
AnaPolianaMesquita <polianamesquit@hotmail.com>
2023-12-21 08:29:47 -0300

    First class: Position

---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,37 @@
+package boardgame;//jogo de tabuleiro
+
+public class Position {//posição
+	private int row;//linha
+	private int column;//coluna
+	
+	public Position(int row, int column) {
+		
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row+","+column;
+	}
+	
+	
+	
+
+}
